8ea37d82cb40d5927bb5fc36e468d2d579a0a970
Douglas Fuelber <contato@douglasfuelber.com>
2021-02-26 22:21:53 -0300

    Created project

---
+++ invoice-api/Controllers/InvoiceController.cs
@@ -0,0 +1,17 @@
+﻿using invoice_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace invoice_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Invoice>> Get()
+        {
+            return Ok(new List<Invoice>());
+        }
+    }
+}
